25fe8b3538405d59dbf6144f2061a0efc2337093
shinhaeng1204@gmail.com <shinhaeng1204@gmail.com>
2025-05-22 14:24:09 +0900

    .

--- src/chatbot.jsx
+++ src/chatbot.jsx
@@ -2,6 +2,29 @@ import React, { useState, useRef, useEffect } from "react";
 import { Send } from "lucide-react" //전송 아이콘
 import PriceTrendChart from "./components/ui/PriceTrendChart"; //가격 추이 그래프 컴포넌트
 import DestinationCard from "./components/ui/DestinationCard"; //목적지 카드 컴포넌트
+import { useParams } from 'react-router-dom';
+
+import NavBar from './components/NavBar';
+import InputArea from './components/InputArea';
+import MessageList from './lib/MessageList';
+
+import useMessage from './lib/useMessage';
+
+export default function ChatPage() {
+	const { id: sessionId } = useParams();
+	const { messageList, addMessage } = useMessage(sessionId);
+
+	return (
+		<div className='flex flex-col w-full h-screen min-w-[1000px]'>
+			<NavBar />
+			<div className='flex-1 overflow-y-auto'>
+				<MessageList messageList={messageList} userId={10} />
+			</div>
+			<InputArea onSend={addMessage} placeholder='메시지를 입력하세요...' />
+		</div>
+	);
+}
+
 export default function ChatBot() {
   const [messages, setMessages] = useState([]);
   const [input, setInput] = useState("");
